9854ba9b63b11729eead8baa8ae2e19c72950900
Anton Mukovozov <graalogosh@gmail.com>
2018-08-18 22:55:39 +0300

    Task model init

---
+++ src/main/java/com/github/graalogosh/DemoApp/models/Task.java
@@ -0,0 +1,41 @@
+package com.github.graalogosh.DemoApp.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Anton Mukovozov (graalogosh@gmail.com) on 18.08.2018.
+ */
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
+    @Getter
+    private Long id;
+
+    @JsonIgnore
+    @Getter
+    private String uuid;
+
+    @Enumerated(EnumType.STRING)
+    @Getter
+    private TaskStatus status;
+
+    @Getter
+    private Date timestamp;
+
+    public Task() {
+        uuid = java.util.UUID.randomUUID().toString();
+        setStatus(TaskStatus.CREATED);
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+        this.timestamp = new Date();
+    }
+}
---
+++ src/main/java/com/github/graalogosh/DemoApp/models/TaskStatus.java
@@ -0,0 +1,10 @@
+package com.github.graalogosh.DemoApp.models;
+
+/**
+ * Created by Anton Mukovozov (graalogosh@gmail.com) on 18.08.2018.
+ */
+public enum TaskStatus {
+    CREATED,
+    RUNNING,
+    FINISHED
+}
